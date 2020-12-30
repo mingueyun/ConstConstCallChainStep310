62be58a56c03a69ec667496c3cc93bbee592511b
student <student@learncodinganywhere.com>
2020-12-30 15:52:39 -0800

    Add project files.

---
+++ ConstConstCallChainStep310/MyData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstConstCallChainStep310
+{
+    public class MyData
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+
+
+
+        public MyData(int yearborn, string lastname)
+        {
+            Age = DateTime.Now.Year - yearborn;
+            Name = lastname;
+            Year = yearborn;
+
+        }
+        public MyData( string lastname): this(1979, lastname)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ ConstConstCallChainStep310/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstConstCallChainStep310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mydata = new MyData("Yun" );
+
+            const string myName = "Min";
+           
+
+            Console.WriteLine("{0} is your first name and  {1} is your last name and you are {2} years old", myName, mydata.Name, mydata.Age);
+
+            Console.ReadLine();
+
+        }
+  
+
+    }
+    
+}
